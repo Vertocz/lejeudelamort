00f392b2d5274f7a8d64977275b2ef58cdc72e01
Vertocz <victor_creze@hotmail.com>
2024-06-14 12:38:40 +0200

    maj jdm

--- JeuDeLaMort/JeuDeLaMort/views.py
+++ JeuDeLaMort/JeuDeLaMort/views.py
@@ -13,7 +13,6 @@ from django.contrib import messages
 
 import requests
 
-
 candidats = Candidat.objects.all()
 users = User.objects.all()
 paris = Pari.objects.all()
@@ -41,9 +40,11 @@ def score_max(joueur, annee):
 def score_user(paris_user, annee):
     score = 0
     for pari in paris_user.filter(saison=annee, mort=True):
+        print(pari.candidat, pari.candidat.points())
         try:
             candidat = pari.candidat
             if candidat.DDD:
+                print(candidat.DDD.year)
                 if candidat.DDD.year == annee:
                     points = candidat.points()
                     score += points
@@ -109,7 +110,8 @@ def salle_user(request, id, annee):
                       {'joueur': joueur, 'poker': joker(joueur, annee), 'score_max': score_max(joueur, annee),
                        'score': score_user(paris_user, annee), 'paris': paris_user, 'deces': paris_decedes,
                        'rap': 10 - len(paris_user) + len(paris_decedes), 'candidats_joueur': candidats_joueur,
-                       'moyenne': moyenne_age(joueur, annee), "terminee": saison_terminee, "saisons": saisons, "saison_en_cours": annee})
+                       'moyenne': moyenne_age(joueur, annee), "terminee": saison_terminee, "saisons": saisons,
+                       "saison_en_cours": annee})
 
     except ObjectDoesNotExist:
         messages.success(request, ("Ce joueur n'existe pas/plus"))
@@ -133,13 +135,14 @@ def classement(request, annee):
         if pari.saison not in saisons:
             saisons.append(pari.saison)
     saisons.sort(key=lambda saison: saison)
-    return render(request, 'jdm/classement.html', {'liste': liste[0], 'en_devenir': liste[1], "saisons": saisons, "saison_en_cours": saison_en_cours})
+    return render(request, 'jdm/classement.html',
+                  {'liste': liste[0], 'en_devenir': liste[1], "saisons": saisons, "saison_en_cours": saison_en_cours})
 
 
 def liste_amis(joueur, saison):
     liste = []
     for user in users:
-        if user.id == 11 and saison != 2023:
+        if user.id == 11 and saison > 2023:
             pass
         else:
             if len(Cercle.objects.filter(joueur=joueur, ami=user)) != 0:
@@ -185,7 +188,7 @@ def nouvel_ami(request, id):
 def retirer_ami(request, id):
     annee = datetime.now().year
     Cercle.objects.get(ami=User.objects.get(id=id), joueur=request.user.id).delete()
-    messages.success(request, (str(User.objects.get(id=id).username)+" a été retiré.e de votre cercle"))
+    messages.success(request, (str(User.objects.get(id=id).username) + " a été retiré.e de votre cercle"))
     return redirect('classement', annee)
 
 
@@ -345,7 +348,7 @@ def resume(request):
     candidats_decedes = Candidat.objects.filter(DDD__isnull=False)
     liste = []
 
-    for year in range(2023, datetime.now().year+1):
+    for year in range(2023, datetime.now().year + 1):
         liste_annee = []
         for candidat in candidats_decedes:
             if candidat.DDD.year == year:
@@ -385,9 +388,12 @@ def maj(request):
             candidat.save()
             for pari in paris:
                 if pari.candidat == candidat:
-                    #pari.mort = True
-                    #pari.save()
+                    pari.mort = True
+                    pari.save()
                     deces.append(candidat)
+                if pari.candidat.DDD and pari.mort == False:
+                    pari.mort = True
+                    pari.save()
 
         if len(deces) > 0:
             for user in users:
@@ -406,9 +412,10 @@ def maj(request):
 
 def maj_annuelle(request):
     for user in users:
-        if len(Historique.objects.filter(joueur=user, saison=int(datetime.now().year)-1)) == 0:
-            Historique(joueur=user, saison=int(datetime.now().year)-1, score=score_user(Pari.objects.filter(joueur=user), int(datetime.now().year)-1), ).save()
-    saison = int(datetime.now().year-1)
+        if len(Historique.objects.filter(joueur=user, saison=int(datetime.now().year) - 1)) == 0:
+            Historique(joueur=user, saison=int(datetime.now().year) - 1,
+                       score=score_user(Pari.objects.filter(joueur=user), int(datetime.now().year) - 1), ).save()
+    saison = int(datetime.now().year - 1)
     for pari in Pari.objects.filter(saison=saison):
         pari.passe = True
         pari.save()
@@ -465,11 +472,13 @@ def pet(request):
                 for prout in prouts:
                     if prout.auteur not in musiciens:
                         musiciens.append(prout.auteur)
-                return render(request, 'jdm/akikileprout.html', {"form": form, 'musiciens': musiciens, 'prouts': prouts})
+                return render(request, 'jdm/akikileprout.html',
+                              {"form": form, 'musiciens': musiciens, 'prouts': prouts})
             else:
                 messages.error(request, 'Ce format de fichier n\'est pas pris en charge')
                 form = ProutForm(initial={'auteur': request.user})
-                return render(request, 'jdm/akikileprout.html', {'form': form, 'musiciens': musiciens, 'prouts': prouts})
+                return render(request, 'jdm/akikileprout.html',
+                              {'form': form, 'musiciens': musiciens, 'prouts': prouts})
         else:
             messages.error(request, 'Quelque chose ne s\'est pas passé correctement')
             form = ProutForm(initial={'auteur': request.user})
@@ -489,8 +498,6 @@ def bingo(request):
         form = JoueurForm(request.POST)
         if form.is_valid():
             joueur = form.data['joueur']
-            print(joueur)
-            print(bingo_joueur(joueur))
             return render(request, 'jdm/bingo.html', {'form': form, 'matches': bingo_joueur(joueur), 'joueur': joueur})
     else:
         form = JoueurForm()
@@ -498,11 +505,10 @@ def bingo(request):
 
 
 def bingo_joueur(joueur):
-    pistols = ["WeapSawnoff_C", "WeapRhino_C", "WeapNagantM1895_C", "WeapM9_C", "WeapM1911_C", "WeapG18_C", "WeapDesertEagle_C"]
     key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI2MjE2MWUwMC0wNDk1LTAxM2QtYmMzYS0zZTk5NDk5ZjNlY2UiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNzE3NTAxMzIyLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImJpbmdvLXB1YmcifQ.Fm-PsL47ALnAjzAU78ER7LZmPRdl8Ye2nrl_J3R45j4"
     URL = "https://api.pubg.com/shards/steam/"
     header = {
-        "Authorization": "Bearer "+key,
+        "Authorization": "Bearer " + key,
         "Accept": "application/vnd.api+json",
     }
 
@@ -518,34 +524,6 @@ def bingo_joueur(joueur):
     if len(jsonresponse['data'][0]['relationships']['matches']['data']) < 10:
         limite = len(jsonresponse['data'][0]['relationships']['matches']['data'])
     for i in range(0, limite):
-        match_id = jsonresponse['data'][0]['relationships']['matches']['data'][i]['id']
-        r = requests.get(URL + match_url + match_id, headers=header)
-        jsonresponse_match = r.json()
-        # récupération de la date
-        date = datetime.strptime(jsonresponse_match['data']['attributes']['createdAt'], '%Y-%m-%dT%H:%M:%SZ').date()
-        gamers = []
-        gamers_du_clan = []
-        classement = 0
-        for dict in jsonresponse_match["included"]:
-            if dict['type'] == 'participant' and 'account' in dict["attributes"]["stats"]["playerId"]:
-                gamer = dict["attributes"]['stats']['name']
-                # on récupère le classement
-                if joueur == gamer and gamer not in gamers_du_clan:
-                    gamers_du_clan.append(gamer)
-                    classement = dict["attributes"]['stats']['winPlace']
-
-                # on établit la liste des joueurs réels
-                if gamer not in gamers:
-                    gamers.append(gamer)
-
-        telemetry = jsonresponse_match['data']['relationships']['assets']['data'][0]['id']
-        for dict in jsonresponse_match["included"]:
-            if dict['id'] == telemetry:
-                telemetry_url = dict["attributes"]["URL"]
-                with urllib.request.urlopen(telemetry_url) as url:
-                    with gzip.open(url, 'r') as fin:
-                        data = json.loads(fin.read().decode('utf-8'))
-
         # 1 Prendre un ramassage d'urgence
         EmPickup = False
         # 2 conduire un ULM
@@ -564,6 +542,7 @@ def bingo_joueur(joueur):
         pistol_kill = False
         # 9 atterrir à Stalber
         stalber = False
+        reynerie = False
         # 10 KOBE
         kobe = False
         # 11 écraser quelqu'un
@@ -580,53 +559,104 @@ def bingo_joueur(joueur):
         used_flashes = 0
         # 16 drive-by kill
         driveby = False
+        # 17 survivre 10 min
+        survivor = False
+        # 18 réanimer 3 fois un teammate
+        camille = False
+
+        positions = []
+
+        match_id = jsonresponse['data'][0]['relationships']['matches']['data'][i]['id']
+        r = requests.get(URL + match_url + match_id, headers=header)
+        jsonresponse_match = r.json()
+
+        # récupération de la date du match
+        date = datetime.strptime(jsonresponse_match['data']['attributes']['createdAt'], '%Y-%m-%dT%H:%M:%SZ').date()
+        gamers = []
+        equipe = []
+
+        telemetry = jsonresponse_match['data']['relationships']['assets']['data'][0]['id']
+
+        game_mode = jsonresponse_match['data']['attributes']['gameMode']
+        team = jsonresponse_match['data']['attributes']['gameMode']
+
+        for dict in jsonresponse_match["included"]:
+            if dict['type'] == 'participant':
+                if 'account' in dict["attributes"]["stats"]["playerId"]:
+                    # on récupère le classement
+                    if joueur == dict["attributes"]['stats']['name']:
+                        classement = dict["attributes"]['stats']['winPlace']
+
+                    # on établit la liste des joueurs réels
+                    if dict["attributes"]['stats']['name'] not in gamers:
+                        gamers.append(dict["attributes"]['stats']['name'])
+
+                if joueur == dict["attributes"]['stats']['name']:
+                    classement = dict["attributes"]['stats']['winPlace']
+                    if dict["attributes"]['stats']['longestKill'] > 100:
+                        kill_100m = True
+                    if dict["attributes"]['stats']['rideDistance'] > 3000:
+                        ride_distance = True
+                    if dict["attributes"]['stats']['roadKills'] > 0:
+                        driveby = True
+                    if dict["attributes"]['stats']['timeSurvived'] > 600:
+                        survivor = True
+                    if dict["attributes"]['stats']['revives'] > 1:
+                        camille = True
+
+            # récupération du json des événements du match
+            if dict['id'] == telemetry:
+                telemetry_url = dict["attributes"]["URL"]
+                with urllib.request.urlopen(telemetry_url) as url:
+                    with gzip.open(url, 'r') as fin:
+                        data = json.loads(fin.read().decode('utf-8'))
 
         for donnee in data:
             try:
-                if donnee['character']['name'] == joueur and donnee['_T'] == 'LogVehicleRide' and donnee['vehicle']['vehicleId'] == 'BP_Motorglider_C' or "BP_Motorglider_Green_C" and donnee['vehicle']['velocity'] > 0 and donnee['vehicle']['seatIndex'] == 1:
-                    ulm = True
+                if donnee['_T'] == 'LogVehicleDamage':
+                    if donnee['attacker']['name'] == joueur and donnee['damageCauserName'] == "Item_Weapon_SpikeTrap_C":
+                        spiked = True
+
+                if donnee['_T'] == "LogPlayerKillV2":
+                    if donnee['killer']['name'] == joueur:
+                        if donnee['victim']['type'] == 'user_ai':
+                            bots_killed += 1
+                        elif donnee['victim']['type'] == 'user':
+                            real_kill = True
+                        if donnee['killerDamageInfo']['damageCauserName'] in ["WeapSawnoff_C", "WeapRhino_C", "WeapNagantM1895_C", "WeapM9_C", "WeapM1911_C", "WeapG18_C", "WeapDesertEagle_C"]:
+                            pistol_kill = True
+                        elif donnee['killerDamageInfo']['damageCauserName'] == "ProjGrenade_C":
+                            kobe = True
+                        elif donnee['killerDamageInfo']['damageCauserName'] == "Damage_VehicleHit":
+                            smash = True
+                        if donnee['victim']['isInBlueZone']:
+                            bz_kill = True
+
+                if donnee['_T'] == "LogVehicleRide":
+                    if donnee['character']['name'] == joueur and 'glider' in donnee['vehicle']['vehicleId']:
+                        ulm = True
+
+                if donnee['_T'] == "LogItemPickupFromCarepackage":
+                    if donnee['character']['name'] == joueur:
+                        crate = True
+
+                if donnee['_T'] == "LogPlayerPosition":
+                    if donnee['character']['name'] == joueur:
+                        positions.append(donnee)
 
                 if donnee['_T'] == "LogEmPickupLiftOff":
-                    for ligne in donnee['riders']:
-                        if ligne['name'] == joueur:
-                            EmPickup = True
-
-                if donnee['_T'] == 'LogItemPickupFromCarepackage' and donnee['character']['name'] == joueur:
-                    crate = True
-
-                if donnee['_T'] == 'LogPlayerKillV2' and donnee['killer'] == joueur and donnee['victim']['type'] == 'user':
-                    real_kill = True
-
-                if donnee['_T'] == 'LogPlayerKillV2' and donnee['killer'] == joueur and donnee['killerDamageInfo']['damageCauserName'] in pistols:
-                    pistol_kill = True
-
-                if donnee['_T'] == 'LogParachuteLanding' and donnee['character']['name'] == joueur and donnee['character']['zone'] == 'stalber':
-                    stalber = True
-
-                if donnee['_T'] == 'LogPlayerKillV2' and donnee['killer'] == joueur and donnee['killerDamageInfo']['damageCauserName'] == "ProjGrenade_C":
-                    kobe = True
-
-                if donnee['_T'] == 'LogPlayerKillV2' and donnee['killer'] == joueur and donnee['killerDamageInfo']['damageCauserName'] == "Damage_VehicleHit":
-                    smash = True
-
-                if donnee['_T'] == 'LogPlayerKillV2' and donnee['killer'] == joueur and donnee['victim']['bz_kill'] == True:
-                    bz_kill = True
-
-                if donnee['_T'] == 'LogPlayerKillV2' and donnee['killer'] == joueur and donnee['victim']['type'] == 'user_ai':
-                    bots_killed += 1
-
-                if donnee['_T'] == 'LogVehicleDamage' and donnee['attacker']['name'] == joueur and donnee['damageCauserName'] == "Item_Weapon_SpikeTrap_C":
-                    spiked = True
-
-                if donnee['_T'] == 'LogItemUse' and donnee['character']['name'] == joueur and donnee['item']['itemId'] == "Item_Weapon_FlashBang_C":
-                    used_flashes += 1
-
-                if donnee['_T'] == 'LogPlayerKillV2' and donnee['killer'] == joueur and donnee['killerDamageInfo']['damageCauserName'] == "Damage_VehicleHit":
-                    driveby = True
+                    for p in range(0, 3):
+                        try:
+                            if donnee['riders'][p]['name'] == joueur:
+                                EmPickup = True
+                        except IndexError:
+                            pass
+                if donnee['_T'] == 'LogItemUse':
+                    if donnee['item']['itemId'] == "Item_Heal_FirstAid_C" and donnee['character']['name'] == joueur:
+                        used_flashes += 1
 
             except KeyError:
                 pass
-
             except TypeError:
                 pass
 
@@ -636,27 +666,25 @@ def bingo_joueur(joueur):
         if used_flashes >= 1:
             flasher = True
 
-        for dict in jsonresponse_match["included"]:
-            if dict['type'] == 'participant' and joueur == dict["attributes"]['stats']['name']:
-                longestKill = dict["attributes"]['stats']['longestKill']
-                if longestKill > 100:
-                    kill_100m = True
-                if dict["attributes"]['stats']['rideDistance'] > 3000:
-                    ride_distance = True
+        if len(positions) < 7:
+            limite = len(positions) - 1
+        else:
+            limite = 6
+        for position in positions:
+            try:
+                # if position['character']['zone'][0] == "school":
+                #     stalber = True
+                if 435000 < position['character']['location']['x'] < 450000 and 326000.0 < position['character']['location']['y'] < 341000 and position['elapsedTime'] < 200:
+                    reynerie = True
+            except IndexError:
+                pass
 
         if classement == 1:
             top1 = True
 
-        # 18
-        # 19
-        # 20
-        # 21
-        # 22
-        # 23
-        # 24
-        # 25
-        bingo = [ulm, top1, EmPickup, crate, kill_100m, ride_distance, real_kill, pistol_kill, stalber, kobe, smash, bz_kill, bots_killer, spiked, flasher, driveby]
-        matches.append([match_id, date, gamers_du_clan, gamers, classement, bingo])
+        bingo = [ulm, top1, EmPickup, crate, kill_100m, ride_distance, real_kill, pistol_kill, reynerie, kobe, smash,
+                 bz_kill, bots_killer, spiked, flasher, driveby, survivor, camille]
+        matches.append([match_id, date, equipe, gamers, classement, bingo, game_mode])
 
     matches.sort(key=lambda x: x[1], reverse=True)
 
